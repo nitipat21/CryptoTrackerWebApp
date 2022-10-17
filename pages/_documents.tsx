d2ed9b08df8aca74974ebc697a15848d2591a46b
nitipat21 <nitipat.rstw@gmail.com>
2022-10-17 11:06:40 +1100

    change next-google-font to built in font optimized

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { GoogleFonts } from "next-google-fonts";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap"/>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
